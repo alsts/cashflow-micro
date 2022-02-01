d30b8a63f1a6ead770ad679b41dbe59d86e38753
Alex <astserba@gmail.com>
2022-02-01 22:44:14 +0000

    Added Common Nuget Library

--- tasks/src/TaskService/Dtos/TaskCreateDto.cs
+++ tasks/src/TaskService/Dtos/TaskCreateDto.cs
@@ -1,8 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using GenericEventDto = Cashflow.Common.Dtos;
 
 namespace TaskService.Dtos
 {
-    public class TaskCreateDto
+    public class TaskCreateDto : GenericEventDto
     {
         [Required] public string Title { get; set; }
         [Required] public string Description { get; set; }
